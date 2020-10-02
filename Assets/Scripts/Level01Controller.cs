f74dba7b7b8f9a6828c575ac5ccae6cc998d272d
Paul Dubberley <pauldubberley@hotmail.com>
2020-10-02 16:12:53 -0500

    Project Initialization + P02A

---
+++ Assets/Scripts/Level01Controller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Level01Controller : MonoBehaviour
+{
+    [SerializeField] Text _currentScoreTextView;
+
+    int _currentScore;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            IncreaseScore(5);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitLevel();
+        }
+    }
+
+    public void ExitLevel()
+    {
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        if(_currentScore > highScore)
+        {
+            PlayerPrefs.SetInt("HighScore", _currentScore);
+            UnityEngine.Debug.Log("New high score: " + _currentScore);
+        }
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void IncreaseScore(int scoreIncrease)
+    {
+        _currentScore += scoreIncrease;
+        _currentScoreTextView.text = "Score: " + _currentScore.ToString();
+    }
+}
